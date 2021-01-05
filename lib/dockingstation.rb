b1a8aee4cd36672e1fcd63550a559c486fbdf91a
jshields123 <jackshields@live.co.uk>
2021-01-05 17:29:22 +0000

    step 8

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,3 +1,5 @@
 class Dockingstation
-
+   def release_bike
+    p "hey"
+   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,3 +1,19 @@
+require 'dockingstation'
+
 describe Dockingstation do
   docking_station = Dockingstation.new
 end
+
+describe 'release bike' do
+  it 'releases bike' do 
+    expect(docking_station.release_bike).to eq "hey"
+  end
+end
+
+
+# describe '#release_bike' do
+# 		it { is_expected.to respond_to :release_bike}
+# 		# Using a '#' before a method name implies that it is an instance method.
+# 		it 'raises an error when there are no bikes available' do
+# 			expect { subject.release_bike }.to raise_error 'No bikes available'
+# 	 	end
